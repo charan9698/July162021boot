ef6b2ddee6b11bcf669a0c1fca2830048c539909
RAMCHARANTEJA <RAMCHARANTEJA@Ramchran>
2021-07-12 23:39:48 +0530

    Minor changes

--- Boot/src/main/java/com/example/service/IEmployeeService.java
+++ Boot/src/main/java/com/example/service/IEmployeeService.java
@@ -1,10 +1,9 @@
 package com.example.service;
 
-import org.springframework.stereotype.Service;
 
 import com.example.model.Employee;
 
-@Service
+
 public interface IEmployeeService {
 	
 	void saveEmployee(Employee E);
